258c58522e7a17a2c84837f131eeac2bb93ae4a5
genesis-g101 <gyreplicant@gmail.com>
2023-11-09 15:14:29 -0300

    aula01 - funções printf e main

--- prog_moderna_c/aula01/hello.c
+++ prog_moderna_c/aula01/hello.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
+#include <stdio.h> // Um cabeçalho é um arquivo que contém funções e comandos.
 
 int main(void) {
-	printf("papobinario.com.br\n");
-	return 0;
+  int ret = 0;
+	ret = printf("papobinario.com.br\n"); //<stdio.h> contém a função printf, que usamos para mostrar a saida na tela.
+  printf("retorno do printf: %d\n", ret);
+  printf("%d - %d - %d - %d\n", ret, 100, 0xa, 'C');
+    
+  return 0; // o zero indica que o prog foi concluido com exito
 }
 
