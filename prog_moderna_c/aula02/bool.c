5960be7a21f32e0a7faab251ac0acd79b4896764
genesis-g101 <gyreplicant@gmail.com>
2023-11-13 19:26:48 -0300

    aula02 - tipo char e bool

--- prog_moderna_c/aula02/bool.c
+++ prog_moderna_c/aula02/bool.c
@@ -4,10 +4,10 @@
 int main(void) {
   bool b;
 
-  printf("O tamanho de b (bool) eh: %lu\n", sizeof b);
+  printf("O tamanho de b (bool) é: %lu byte\n", sizeof b);
 
   b = false;
-  printf("O valor de b eh: %i\n", b);
-
-  return 0;
+  printf("O valor inteiro de b é %i\n", b);
+  // 0 é falso
+  // 1 é verdadeiro
 }
